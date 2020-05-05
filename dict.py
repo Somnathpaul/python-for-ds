86a3d2109863cadd7170e0078ff1afb0d234bf4c
Somnath Paul <Somnath4x@gmail.com>
2020-05-05 19:48:25 +0530

    modified

--- dict.py
+++ dict.py
@@ -14,3 +14,26 @@ count = {}
 for n in name:
     count[n] = count.get(n, 0) + 1
 print(count)
+
+# a program to find the repetition of words in a sentence
+define_python= "In Python, a function is a group of related statements that performs a specific task.Functions help break our program into smaller and modular chunks. As our program grows larger and larger, functions make it more organized and manageable."
+hash_map={}
+lst = define_python.split()
+print(lst)
+for i in lst:
+    hash_map[i] = hash_map.get(i,0) +1
+print(hash_map)
+
+names = {'jack': 1, 'brad': 2, 'jill': 3}
+for n in names:
+    # n prints names key
+    # names[n] prints number values
+    print(n, names[n])
+
+# built in functions
+print(names.keys())
+print(names.values())
+
+# special case for loop for dict
+for k,v in names.items():
+    print(k,v)
